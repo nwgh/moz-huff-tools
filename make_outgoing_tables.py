8a25ca47dc58fbe506dc3d67d08b7f8e8f00c059
Nicholas Hurley <hurley@todesschaf.org>
2013-10-31 13:30:55 -0700

    Initial commit - HPACK draft04 tables

---
+++ make_outgoing_tables.py
@@ -0,0 +1,48 @@
+import sys
+
+sys.stdout.write('''#ifndef mozilla__net__Http2HuffOutgoing_h
+#define mozilla__net__Http2HuffOutging_h
+
+namespace mozilla {
+namespace net {
+
+struct huff_outgoing_entry {
+  uint8_t length;
+  uint32_t value;
+};
+
+huff_outgoing_entry *huff_outgoing = {
+''')
+
+entries = []
+for line in sys.stdin:
+    line = line.strip()
+    obracket = line.rfind('[')
+    nbits = int(line[obracket + 1:-1])
+
+    encend = obracket - 1
+    hexits = nbits / 4
+    if hexits * 4 != nbits:
+        hexits += 1
+
+    enc = line[encend - hexits:encend]
+    val = int(enc, 16)
+
+    entries.append({'length': nbits, 'value': val})
+
+line = []
+for i, e in enumerate(entries):
+    sys.stdout.write('  { .length = %s, .value = 0x%08x }' %
+                     (e['length'], e['value']))
+    if i < (len(entries) - 1):
+        sys.stdout.write(',')
+    sys.stdout.write('\n')
+
+sys.stdout.write('''};
+
+} // namespace net
+} // namespace mozilla
+
+#endif // mozilla__net__Http2HuffOutgoing_h
+''')
+
